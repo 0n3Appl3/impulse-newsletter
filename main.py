dd211d6849d06ae4a3832b79a6c4215cef46a665
0n3Appl3 <jedd.lupoy@gmail.com>
2024-05-09 12:42:03 +1200

    Implemented ability to send HTML emails
    Can set custom newsletter subjects and text. No database or website yet.

---
+++ main.py
@@ -0,0 +1,49 @@
+import smtplib
+from email.mime.text import MIMEText
+from bs4 import BeautifulSoup
+
+port = 465
+smtp_server = "smtp.gmail.com"
+
+sender_name = "Impulse Newsletter"
+sender_email = "automail.newsletter@gmail.com"
+sender_app_password = "hqgs hwkk iani pkvx"
+
+receiver_email = "jedd.lupoy@gmail.com"
+
+template = open('template.html')
+soup = BeautifulSoup(template.read(), "html.parser")
+
+email_template = soup.find('section')
+html_start = str(soup)[:str(soup).find(str(email_template))].replace('\n', '')
+html_end = str(soup)[str(soup).find(str(email_template)) + len(str(email_template)):].replace('\n', '')
+
+#########################
+# Temporary
+subject = "The Nature of Choice"
+text = "Ah, choice! The grand illusion that dances before you like a marionette on strings. You see, dear player, your decisions matter—or so you believe. The narrator’s voice guides you, nudging you toward doors, buttons, and existential musings. But is it truly choice when the outcome is predetermined? When the walls echo with laughter, mocking your futile rebellion?<br><br>Observe the branching paths—the tantalizing corridors that beckon. Left or right? Blue or red? Freedom or conformity? You choose, and yet, the threads weave back together, mocking your defiance. The narrator chuckles, for he knows the truth: you are but a rat in his maze, chasing breadcrumbs of agency. The illusion of choice, my dear player, is the cruelest trick of all."
+#########################
+
+newsletter_title = email_template.p
+newsletter_title.string = subject
+
+newsletter_body = email_template.div.p
+newsletter_body.clear()
+newsletter_body.append(BeautifulSoup(text, "html.parser"))
+
+newsletter_content = str(email_template)
+
+body = html_start + newsletter_content + html_end
+
+message = MIMEText(body, 'html')
+message['Subject'] = subject
+message['From'] = sender_name
+message['To'] = receiver_email
+
+def main():
+    with smtplib.SMTP_SSL(smtp_server, port) as server:
+        server.login(sender_email, sender_app_password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+if __name__ == "__main__":
+    main()
