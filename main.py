71d706498ab7ba98d296ac20c1995cd54468f06d
0n3Appl3 <jedd.lupoy@gmail.com>
2024-05-13 15:19:29 +1200

    Implemented task scheduling
    Checks for unsent newsletters to post every day.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import smtplib
 import requests
+import time
+import schedule
+from datetime import datetime, date
 from progress.bar import ChargingBar
 from email.mime.text import MIMEText
 from bs4 import BeautifulSoup
@@ -39,7 +42,7 @@ def main():
     """
     content = has_an_unsent_newsletter()
     if (content == False):
-        return print("No new newsletter to send.")
+        return print("[{}] No new newsletter to send".format(get_current_date_time()))
 
     get_all_emails_request = requests.get(url = ALL_EMAILS_ENDPOINT)
     response = get_all_emails_request.json()
@@ -47,9 +50,11 @@ def main():
     bar = ChargingBar("Sending Newsletter", max = len(response))
     bar.next()
 
+    # Log into the email newsletter account
     server = smtplib.SMTP_SSL(SMTP_SERVER, PORT)
     server.login(SENDER_EMAIL, SENDER_APP_PASSWORD)
 
+    # Populate the newsletter template
     newsletter_title = email_template.article.p
     newsletter_title.string = content['title']
 
@@ -60,6 +65,7 @@ def main():
     newsletter_image = email_template.div.img
     newsletter_image['src'] = content['image']
 
+    # Send the newsletter to all subscribers
     for recipient in response:
         # Unsubscribe link is created for each recipient
         newsletter_unsubscribe = email_template.footer.p.a
@@ -79,7 +85,8 @@ def main():
 
     set_posted_newsletter_request = requests.get(url = SET_SENT_ENDPOINT)
     if (set_posted_newsletter_request.status_code != 200):
-        return print("\nAn error has occurred and the newsletter has not been set as posted.")
+        return print("\n[{}] An error has occurred and the newsletter has not been set as posted".format(get_current_date_time()))
+    print("\n[{}] Newsletter sent to all subscribers".format(get_current_date_time()))
     bar.finish()
 
 def has_an_unsent_newsletter():
@@ -93,5 +100,20 @@ def has_an_unsent_newsletter():
         return False
     return response
 
+def get_current_date_time():
+    """
+    Returns the current date and time.
+    """
+    current_date = date.today()
+    current_time = datetime.now()
+    return str(current_date) + " " + current_time.strftime("%H:%M:%S")
+
 if __name__ == "__main__":
-    main()
+    print("██████████  Impulse Newsletter Python Script  ██████████")
+    print("[{}] Script started".format(get_current_date_time()))
+    schedule.every().day.at("08:00").do(main)
+
+    # Continuously run tasks that are scheduled to run
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
